8a2bd4645d6f147cb2d1a0e6be66036c6a342864
vladubogdan <vladubogdan@4282601a-d46c-bd12-f361-567c96ba3fb4>
2012-07-30 10:46:33 +0000

    18 Jul 2012
    First release for LH 1.4.9X
    Not ready for public use. 
    I'm just saving my progress and test everything out. 
    Not all features implemented.

---
+++ CustomClasses/TemporaryDummyClass.cpp
@@ -0,0 +1,16 @@
+//THIS CLASS WAS ADDED TO CANCEL SOME WARNINGS UNTIL LH 1.4.953 gets released
+//on both website and Mac App Store. It will be removed after.
+
+#include "TemporaryDummyClass.h"
+#include "LHDictionary.h"
+#include "LHArray.h"
+#include "LHObject.h"
+
+TemporaryDummyClass::~TemporaryDummyClass(){
+
+}
+
+void TemporaryDummyClass::setPropertiesFromDictionary(LHDictionary* dictionary){
+
+}
+
